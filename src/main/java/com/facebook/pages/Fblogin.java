1f033cf527f234ea70379907bca339265aa9404b
delsonroy7 <43088529+delsonroy7@users.noreply.github.com>
2018-09-08 16:26:35 +0530

    commitfacebook1

---
+++ src/main/java/com/facebook/pages/Fblogin.java
@@ -0,0 +1,15 @@
+package com.facebook.pages;
+
+import org.testng.annotations.Test;
+
+public class Fblogin {
+	
+	@Test
+	void loginSetup()
+	{
+		
+		System.out.println("set up has been done");
+		
+	}
+
+}
